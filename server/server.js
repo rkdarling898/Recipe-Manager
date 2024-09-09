204de7b1925f1738e8c56da52720567c28d6fbca
rkdarling898 <rkdarling898@gmail.com>
2024-09-08 20:23:28 -0400

    Chose to use Fastify for server to connect to MySql database. Created enviroment variables.

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import Fastify from 'fastify'
+import mysql from 'mysql'
+
+const fastify = Fastify({
+  logger: true
+})
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD
+})
+
+fastify.get('/', async function handler (req, res) {
+  connection.connect(err => {
+    if (err) throw err
+
+    return { message: 'Connection failure' }
+  })
+
+  return { message: 'Connection success'}
+})
+
+try {
+  await fastify.listen({ port: process.env.PORT })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
