f9eb0dcc2ccd33f459a65c0086ed7ca34aae9234
rkdarling898 <reillydarling@icloud.com>
2024-09-15 22:41:52 -0400

    Set puppeteer to run in shell mode

--- server/js/scraper.js
+++ server/js/scraper.js
@@ -2,12 +2,15 @@ import puppeteer from 'puppeteer';
 import { getProperty } from './scraper-utils.js';
 
 export async function scrapeRecipe () {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({headless: 'shell'})
     const page = await browser.newPage()
 
-    await page.goto('https://www.allrecipes.com/recipe/13309/rich-and-simple-french-onion-soup/') // Example site
+    await page.goto('https://www.allrecipes.com/copycat-cracker-barrel-hash-brown-casserole-shepherds-pie-recipe-8706205') // Example site
 
     // Main code
+    const title = await getProperty(page, 'h1.article-heading.type--lion', 'textContent')
+
+    console.log(title)
 
     await browser.close()
 }
